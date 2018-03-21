ab81e356024366f9c1dd3fc362db8220c95ed11d
MinusGix <MinusGix@Gmail.com>
2018-03-20 23:17:02 -0500

    Added stackexchange with comments and the original question to be displayed

--- modules/stackexchange.js
+++ modules/stackexchange.js
@@ -1,5 +1,33 @@
 module.exports = {
 	init: (Data) => {
+		Data.app.get('/stackexchange/question/:answerID', (req, res) => {
+			console.log('boo');
+			Data.request('getstackexchangequestion&answers ' + req.params.answerID, data => {
+				let question = {
+					comments: data.comments || [],
+					link: data.link,
+					title: data.title,
+					body: data.body,
+					score: data.score
+				};
+				let answers = data.answers.map(answer => {
+					return {
+						score: data.score || 0,
+						body: answer.body || '',
+						comments: (answer.comments || [])
+							.map(comment => comment.body),
+						accepted: data.is_accepted || false
+					};
+				});
+
+				res.render('stackexchange.ejs', {
+					answers,
+					question
+				});
+				console.log('hi', question, answers);
+			});
+		});
+		
 		Data.app.get('/stackexchange/question/answers/:questionID', (req, res) => {
 			Data.request("getstackexchangeanswers " + req.params.questionID, data => {
 				let answers = data.map(answer => {
@@ -7,7 +35,7 @@ module.exports = {
 					let accepted = answer.is_accepted;
 					let poster = {
 						name: answer.owner.display_name,
-						profileURL: answer.owner.linke
+						profileURL: answer.owner.link
 					};
 
 					return {
